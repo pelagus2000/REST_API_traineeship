a0f76baa08393b94a04bed5c291510414f24c0a9
pelagus2000 <pelagus2012@gmail.com>
2025-04-28 16:52:29 +0300

    *добавлена ссылка на условия пользования. *добавлен детальный перевал  и юзер *определены взаимодействия туриста и модератора с моделями для этого

---
+++ pereval/signals.py
@@ -0,0 +1,23 @@
+# pereval/signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Pereval, ModerationComment, Notification
+
+@receiver(post_save, sender=Pereval)
+def create_status_notification(sender, instance, created, **kwargs):
+    if not created and 'status' in kwargs.get('update_fields', []):
+        status_display = instance.get_status_display()
+        Notification.objects.create(
+            pereval=instance,
+            notification_type='status_change',
+            message=f'Статус вашего перевала "{instance.title}" изменен на "{status_display}".'
+        )
+
+@receiver(post_save, sender=ModerationComment)
+def create_comment_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(
+            pereval=instance.pereval,
+            notification_type='comment',
+            message=f'Модератор оставил комментарий к вашему перевалу "{instance.pereval.title}".'
+        )
